813c6bdea57b6756b5437ed3ce1442fcfac064d9
Antcar <ansonchung929@outlook.com>
2023-02-04 22:32:57 +0800

    Client Core

--- src/Client/QuestServiceClient.lua
+++ src/Client/QuestServiceClient.lua
@@ -1,1 +1,21 @@
----
+--[=[
+    @class QuestServiceClient
+
+    Questing, but Client!
+    (c) 2023 Bloxcode A/P Antcar
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local QuestServiceClient = {}
+QuestServiceClient.ServiceName = "QuestServiceClient"
+
+function QuestServiceClient:Init(serviceBag)
+    self._serviceBag = assert(serviceBag, "QuestServiceClient/ ServiceBag is nil")
+
+    print("QuestServiceClient/ Initializing...")
+
+
+
+    print("QuestServiceClient/ Initialized!")
+end
