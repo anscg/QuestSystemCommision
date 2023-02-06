e9754d3196ee43653ccc67e910f86bc6b513c9e7
Antcar <ansonchung929@outlook.com>
2023-02-06 23:15:00 +0800

    Basic Quest Screen

--- src/Client/_Ui/Quest/QuestScreen.lua
+++ src/Client/_Ui/Quest/QuestScreen.lua
@@ -6,3 +6,34 @@
 ]=]
 
 local require = require(script.Parent.Loader).load(script)
+
+local BasicPane = require("BasicPane")
+local Blend = require("Blend")
+local SpringObject = require("SpringObject")--Should be using BaseWindow not BasicPane!!!!!
+local BaseWindow = require("BaseWindow")
+
+local QuestScreen = setmetatable({}, BaseWindow)
+QuestScreen.__index = QuestScreen
+QuestScreen.ClassName = "QuestScreen"
+
+function QuestScreen.new(obj)--HMM
+    local self = setmetatable(BaseWindow.new(obj), QuestScreen)
+
+    --Doing this later
+    self:SetDisplayName("Quests!")
+
+    self._maid:GiveTask(self._render:Subscribe(function(gui)
+        self.Gui = gui
+    end))
+
+    return self
+end
+
+function QuestScreen:_render()
+    return self:_renderBase({
+
+        })
+end
+
+return QuestScreen
+
