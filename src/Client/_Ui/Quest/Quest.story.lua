5115abbc498e18fd682de5ea7d1326ee62f31cb9
Antcar <ansonchung929@outlook.com>
2023-02-06 21:16:50 +0800

    Evenmore!?

---
+++ src/Client/_Ui/Quest/Quest.story.lua
@@ -0,0 +1,19 @@
+local require = require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).load(script)
+
+local Maid = require("Maid")
+local StoryBarPaneUtils = require("StoryBarPaneUtils")
+local StoryBarUtils = require("StoryBarUtils")
+local QuestScreen = require("QuestScreen")
+
+return function(target)
+    local maid = Maid.new()
+
+    local questScreen = QuestScreen.new()
+    questScreen.Gui.Parent = target
+    questScreen:SetDisplayName("Quests!")
+    questScreen:Show()
+    maid:GiveTask(questScreen)
+
+    local bar = StoryBarUtils.createStoryBar(maid, target)
+    StoryBarPaneUtils.makeVisibleSwitch(bar, questScreen)
+end
---
+++ src/Client/_Ui/Quest/QuestScreen.lua
@@ -0,0 +1,8 @@
+--[=[
+    @class QuestScreen
+
+    Questing Screeneeen!
+    (c) Bloxcode 2023
+]=]
+
+local require = require(script.Parent.Loader).load(script)
